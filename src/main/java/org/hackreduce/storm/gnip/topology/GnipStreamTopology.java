097f6161d15b0984118c0643e53aeebae2059d74
Alex Arutyunyants <aarutyunyants@visiblemeasures.com>
2013-07-16 18:28:13 -0400

    refactoring submit-topo to submit to cluster or to run local (when args specified)

--- src/main/java/org/hackreduce/storm/gnip/topology/GnipStreamTopology.java
+++ src/main/java/org/hackreduce/storm/gnip/topology/GnipStreamTopology.java
@@ -2,6 +2,10 @@ package org.hackreduce.storm.gnip.topology;
 
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.InvalidTopologyException;
+import backtype.storm.generated.StormTopology;
 import backtype.storm.topology.TopologyBuilder;
 import org.hackreduce.storm.gnip.bolt.GnipEdcRequestBolt;
 import org.hackreduce.storm.gnip.bolt.GnipEventPersistBolt;
@@ -9,8 +13,7 @@ import org.hackreduce.storm.gnip.bolt.GnipEventTransformBolt;
 
 public class GnipStreamTopology {
 
-  public static void main(String[] args) {
-
+  public static void submitTopology(LocalCluster cluster) throws InterruptedException, AlreadyAliveException, InvalidTopologyException {
     GnipEdcRequestBolt gnipEdcRequestBolt = null;
     gnipEdcRequestBolt = new GnipEdcRequestBolt(
         "https://bostonstorm.gnip.com/data_collectors/1/activities.xml",
@@ -27,15 +30,35 @@ public class GnipStreamTopology {
     builder.setBolt("gnip_persist", new GnipEventPersistBolt(), 3).localOrShuffleGrouping("gnip_transform", "msgs");
 
     Config conf = new Config();
+    StormTopology topology = builder.createTopology();
+    if (cluster != null) {
+      submitLocalTopology(cluster, "GNIP_EDC_Topology", conf, topology);
+    } else {
+      StormSubmitter.submitTopology("GNIP_EDC_Topology", conf, topology);
+    }
+  }
 
-    LocalCluster cluster = new LocalCluster();
-    cluster.submitTopology("GNIP_EDC_Topology", conf, builder.createTopology());
+  private static void submitLocalTopology(LocalCluster cluster, String topoName, Config conf, StormTopology topology) throws InterruptedException {
+    cluster.submitTopology("GNIP_EDC_Topology", conf, topology);
     try {
       Thread.sleep(30 * 60 * 1000); //30 min
+    } finally {
+      cluster.killTopology("GNIP_EDC_Topology");
+      cluster.shutdown();
+    }
+  }
+
+  public static void main(String[] args) {
+    try {
+      //if there is any command-line parameter - will run local cluster
+      GnipStreamTopology.submitTopology(((args != null && args.length > 0)) ? new LocalCluster() : null);
     } catch (InterruptedException e) {
       e.printStackTrace();
+    } catch (AlreadyAliveException e) {
+      e.printStackTrace();
+    } catch (InvalidTopologyException e) {
+      e.printStackTrace();
     }
-    cluster.killTopology("GNIP_EDC_Topology");
-    cluster.shutdown();
   }
+
 }
