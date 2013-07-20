c4d3463792167ab5e4da75dd2da3a29c454dd593
Alex Arutyunyants <aarutyunyants@visiblemeasures.com>
2013-07-20 00:09:37 -0400

    consume two GNIP feeds; masking GNIP login

--- src/main/java/org/hackreduce/storm/gnip/topology/GnipStreamTopology.java
+++ src/main/java/org/hackreduce/storm/gnip/topology/GnipStreamTopology.java
@@ -16,19 +16,29 @@ import org.hackreduce.storm.gnip.bolt.GnipEventTransformBolt;
 public class GnipStreamTopology {
 
   public static void submitTopology(LocalCluster cluster) throws InterruptedException, AlreadyAliveException, InvalidTopologyException {
-    GnipEdcRequestBolt gnipEdcRequestBolt = null;
-    gnipEdcRequestBolt = new GnipEdcRequestBolt(
+    GnipEdcRequestBolt gnipEdcYoutubeRequestBolt = null;
+    gnipEdcYoutubeRequestBolt = new GnipEdcRequestBolt(
         "https://bostonstorm.gnip.com/data_collectors/1/activities.xml",
-        "aarutyunyants",
-        "St0rmH4ck2013"
+        "xxx",
+        "xxx"
+    );
+
+    GnipEdcRequestBolt gnipEdcTwitterRequestBolt = null;
+    gnipEdcTwitterRequestBolt = new GnipEdcRequestBolt(
+        "https://bostonstorm.gnip.com/data_collectors/4/activities.xml",
+        "xxx",
+        "xxx"
     );
 
     Config tickConfig = new Config();
     tickConfig.put(Config.TOPOLOGY_TICK_TUPLE_FREQ_SECS, 1);
 
     TopologyBuilder builder = new TopologyBuilder();
-    builder.setBolt("gnip_in", gnipEdcRequestBolt, 1).addConfigurations(tickConfig);
-    builder.setBolt("gnip_transform", new GnipEventTransformBolt(), 2).localOrShuffleGrouping("gnip_in", "gnip_events");
+    builder.setBolt("gnip_in_yt", gnipEdcYoutubeRequestBolt, 1).addConfigurations(tickConfig);
+    builder.setBolt("gnip_in_tw", gnipEdcTwitterRequestBolt, 1).addConfigurations(tickConfig);
+    builder.setBolt("gnip_transform", new GnipEventTransformBolt(), 2)
+        .localOrShuffleGrouping("gnip_in_yt", "gnip_events")
+        .localOrShuffleGrouping("gnip_in_tw", "gnip_events");
     builder.setBolt("gnip_persist", new GnipEventPersistBolt(), 3).localOrShuffleGrouping("gnip_transform", "msgs");
 
     Config conf = new Config();
@@ -52,6 +62,7 @@ public class GnipStreamTopology {
 
   public static void main(String[] args) {
     try {
+      System.out.println("Starting");
       //if there is any command-line parameter - will run local cluster
       GnipStreamTopology.submitTopology(((args != null && args.length > 0)) ? new LocalCluster() : null);
     } catch (InterruptedException e) {
